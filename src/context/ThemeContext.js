602736cd07ab441adb17bd6c939c70ec133f84db
Venkateshthullibilli <venkysrinu1999@gmail.com>
2025-04-28 14:14:33 +0530

    Update ThemeContext.js

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,28 +1,8 @@
-// import { createContext,useState,useContext, Children } from "react";
-
-// const ThemeContext = createContext()
-
-// export const ThemeProvider = ({Children}) => {
-//     const [theme,setTheme] = useState(false)
-
-//     const changeTheme = () => {
-//         setTheme(prevState => !prevState)
-//     }
-//     return (
-//         <ThemeContext.Provider value={{theme,changeTheme}}>
-//             {Children}
-//         </ThemeContext.Provider>
-//     )
-
-// }
-
-// export const useTheme = () => useContext(ThemeContext)
-
 import React, { createContext, useState, useContext } from "react";
 
 const ThemeContext = createContext();
 
-export const ThemeProvider = ({ children }) => { // Corrected children prop
+export const ThemeProvider = ({ children }) => { 
     const [theme, setTheme] = useState(false);
 
     const changeTheme = () => {
